1343bcb7db825597dad352c0d12e7d5b58ea0e58
Zach <18583032+IronCityCoder@users.noreply.github.com>
2019-06-13 19:31:44 -0500

    Glass 0.0.1 official.
    Hoping formatting works. Also need to document it...

--- glass.py
+++ glass.py
@@ -18,33 +18,40 @@ except ImportError:
           - https://github.com/ahupp/python-magic")
 
 try:
-    import iosquery
+    from iosquery import query
 
+except ImportError:
+    print("The iosqueries are not in the same directory! \n\
+           Please make sure to add them to run the program!")
 
-'''The glass.py script will traverse through a phone backup and look for database files. 
-It will then open these files up in sqlite 3 and attempt to execute multiple queries. 
-The output of each query will be stored in a file that is inside of a new folder made by the script each execution. 
+'''
+#The glass.py script will traverse through a phone backup and look for database files. 
+#It will then open these files up in sqlite 3 and attempt to execute multiple queries. 
+#The output of each query will be stored in a file that is inside of a new folder made by the script each #execution. 
 
-Naming format: [date][root]/[db][query].csv
+#Naming format: [date][root]/[db][query].csv
 
-Example:
-June-05-19-zachbackup/glassdoorSELECTcompany.csv
-June-07-19-zachbackup/glassdoorJOINnotifications.csv
-June-12-19-zachbackup2/glassdoorSELECTtimestamps.csv
+#Example:
+#2019-5-07-zachbackup/glassdoorSELECTcompany.csv
+#2019-6-04-zachbackup/glassdoorJOINnotifications.csv
+#2019-6-05-zachbackup2/glassdoorSELECTtimestamps.csv
 
-Dependencies: 
-pip install python-magic'''
+#Dependencies: 
+#pip install python-magic
+'''
 
-def processDirectories(directory) :
-    for root, dirs, files in os.walk(directory) :
+#This function walks through the direectories to find database files.
+def processDirectories(directory):
+    for root, dirs, files in os.walk(directory):
         filecount = 0
         dbfiles = 0
         for file in files:
             fname = os.path.join(root, file)
             #Check if ftype is a sqlite file.
-            m = magic.Magic(mime=True)
+            #If mime is true we need another condition to check for.
+            m = magic.Magic(mime=False)
             ftype = m.from_file(os.path.abspath(fname))
-            if "SQLite 3.x database" in ftype:
+            if "SQLite 3.x" in ftype:
                 result = runthrough(fname, file)
             else:
                 result = False
@@ -60,52 +67,70 @@ def processDirectories(directory) :
 def runthrough(dbpath, db):
     if os.path.exists(dbpath):
         #Grabs our pre-made select statements.
-        if "glassdoor" in dbpath:
-            for i in iosquery.SELECT:
-                dbexec(db, i)
+        #print("PATH IS REAL")
+        #print(dbpath)
+        if "glassdoor" in dbpath.lower():
+            for i in query.getSelect():
+                dbexec(db, i, os.path.abspath(dbpath))
+            return True
+
         else:
             return False
     else:
         return False
 
-def dbexec(db, command):
+def dbexec(db, command, fullpath):
     #Runs commands on the database.
     #Open db
+    #print(os.getcwd())
     try:
-        conn = sqlite3.connect(db)
-        cursor = conn.cursor()
-    except:
-        print("{} could not be opened!".format(db))
+        #print('{}'.format(fullpath))
+        #print(os.getcwd)
+        #print(command)
+        conn = sqlite3.connect('{}'.format(fullpath))
+        
+        #Run command
+        #If successful output to csv
+        #Filedate is HourMinute
+        #Else write to error log
+        curr = conn.cursor()
+        output = []
+        for row in curr.execute(command):
+            #print(row)
+            output.append(row)
 
-    #Run command
-    #If successful output to csv
-    #Filedate is HourMinute
-    #Else write to error log
-    try:
-        output = cursor.execute(command)
+        conn.close()
+        #print(output)
         #We have some complex string formatting.
         #Since the queries can contain a * 
         #our dynamic naming options will be limited.
+        #also makes the directory
         dirDate = str(datetime.datetime.now()).split(" ")[0]
-        dirName = "{}-{}".format(fileDate, directory)
+        dirDesc = str(db.split(" ")[-1].split(".")[0])
+        dirName = "{}-{}".format(dirDate, dirDesc)
+        try:
+            os.mkdir(dirName)
+        except FileExistsError:
+            print("{} Directory already exists".format(dirName))
         #example: 2019-06-11-glassdoor
         fileDate =  str(datetime.datetime.now()).split(" ")[1].split(":")[:2]
         fileDate = fileDate[0] + fileDate[1]
         fileCommand = command.split(" ")[:1][0]
-        fileName = "{}{}{}".format(db, fileCommand, fileDate)
-        with open("{}\\{}.csv".format(dirName, fileName), "rw") as newfile:
-            newFile.write(output)
+        fileName = "{}{}{}".format(dirDesc, fileCommand, fileDate)
+        #print("{}/{}.csv".format(dirName, fileName))
+        with open("{}/{}.csv".format(dirName, fileName), "w+") as newFile:
+            for line in output:
+                newFile.write(str(line))
     except:
         #Errlog will be an existing file that we keep adding to
         #Might change this implementation later on if too crowded
         #or purge it after each analysis
-        with open("errorlog.csv") as errlog:
+        with open("errorlog.csv", "w+") as errlog:
             errlog.write("Error with database: {} performing command: {}".format(db, command))
-        return False
 
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     #Grab our arguments. 
     #Script is the name of our script
     #Directory is the name of the directory to start traversal.
