3283df7e63e7520d4627b16d3528c87c82185f73
Zach <18583032+IronCityCoder@users.noreply.github.com>
2019-07-22 14:03:20 -0500

    Manual mode updated
    Handles .list and .table and .quit

--- glass.py
+++ glass.py
@@ -25,6 +25,7 @@ import math
 import argparse
 import sys
 
+### FILE OPERATIONS
 # Open up our queries file.
 def open_queries():
 	query = []
@@ -33,8 +34,14 @@ def open_queries():
 			for command in row.split(","):
 				query.append(command.strip())
 	return query
-			 
+
+# Checking for duplicates before appending new sql commands to file.			 
 def write_queries(command):
+	with open("queries/iosquery.csv", "r+") as iosquery:
+		for line in iosquery:
+			if command in line:
+				return
+
 	with open("queries/iosquery.csv", "a") as iosquery:
 		iosquery.write(f"{command},")
 
@@ -43,6 +50,19 @@ def log_error(message):
 	with open("errorlog.csv", "a") as errlog:
 		errlog.write(f"{dt.now()}{message}\n")
 
+# Handles query output to file.
+def auto_report(db_response):
+	filename = generate_name(db, command)
+
+	# Open a file and write the command output to it.
+	with open(f"{filename}.csv", "w+") as output_file:
+		try:
+			for row in db_response:
+				output_file.write(str(row))  
+		except:
+			log_error(f"Error with running {command} on {db}")
+
+### UTILITIES
 # Uses pathlib's rglob function to search fstrings for files that have keyword in them.
 def pattern(keyword, extension, pathObj):
 	file_grab = pathObj.rglob(f"*.{extension}")
@@ -64,16 +84,21 @@ def crawl(directory):
 	dbs = sqlite_db + regular_db  # Store all the matches in a list.
 	return dbs
 
-# Cycles through every db found and running queries against it.
-def run_through(dbs):
+# Will display a list of found dbs.
+def display_db(dbs):
+	common_names = []
 	for count, file in enumerate(dbs):
-		progress = math.floor((count / len(dbs)) * 100)
-		print(f"{progress}% of the way done")
-		# Iterate over our pre-made queries.
-		for query in open_queries():
-			db_exec(file, query)
-	       
-	print(f"{len(dbs)} database files found!")
+		path = pathlib.Path(file)
+		common_names.append(path.name)
+		print(f"{count}|{path.name}")
+	return common_names
+
+# Using string formatting to create a name for our results.
+def generate_name(db, command):
+	date = dt.now()
+	filedate = f"{date.day}-{date.month}-{date.year}"
+	command = command.split(" ")[:1][0]
+	return f"{keyword}{command}{date}"
 
 # Returns an opened database object.
 # If fails we log an error.
@@ -85,81 +110,69 @@ def db_connect(db):
 	except:
 		log_error(f"Error opening database: {db}!")
 
-# Using string formatting to create a name for our results.
-def generate_name(db, command):
-	date = dt.now()
-	filedate = f"{date.day}-{date.month}-{date.year}"
-	command = command.split(" ")[:1][0]
-	return f"{keyword}{command}{date}"
+def sqlite_tables(db):
+	table = "SELECT name FROM sqlite_master WHERE type='table';"
+	resp = db_exec(db, table)
+	for row in resp:
+		print(str(row))
 
 # Try to open the database file.
 # If successful it will pass a query.
 # Otherwise an error will be logged.
 
-### Might want to generalize this and write output in a different function ###
-### - write_report() ?
-
+## TODO handle when the connection closes
 def db_exec(db, command):
-	db_obj = db_connect(db) # Db object
-	curr = db_obj[0]
-	filename = generate_name(db, command)
-
-	# Open a file and write the command output to it.
-	with open(f"{filename}.csv", "w+") as output_file:
-		try:
-			for row in curr.execute(command):
-				output_file.write(str(row))  
-		except:
-			log_error(f"Error with running {command} on {db}")
-
-	db_obj[1].close()  # Close the database
-
-
-# Manually interacting with the database.
-def display_db(dbs):
-	common_names = []
-	for count, file in enumerate(dbs):
-		path = pathlib.Path(file)
-		common_names.append(path.name)
-		print(f"{count}|{path.name}")
-	return common_names
-
-#Command input.
-def manual_db(database, common_name, path):
-	db_obj = db_connect(database)
+	db_obj = db_connect(db) #Db object
 	curr = db_obj[0]
 	conn = db_obj[1]
-	print(f"Connected successfully to {database}")
+	response = curr.execute(command)
+	#conn.close()  # Close the database
+	return response
+
+### AUTO MODE
+# Cycles through every db found and running queries against it.
+def run_through(dbs):
+	for count, file in enumerate(dbs):
+		progress = math.floor((count / len(dbs)) * 100)
+		print(f"{progress}% of the way done")
+		# Iterate over our pre-made queries.
+		for query in open_queries():
+			response = db_exec(file, query)
+			auto_report(response)
+	       
+	print(f"{len(dbs)} database files found!")
+
+### MANUAL MODE
+# manual_db allows the user to send commands to the db.
+# Will try to re-create some functionality.
+def manual_db(database, common_name, path, dblist):
+	print(f"Connected to {database}")
 	while True:
 		response = input(f"{common_name}> ")
 		if response == ".quit":
 			print("Quitting the program.")
-			conn.close()
 			sys.exit()
 		elif response == ".list":
-			manual_mode(path)
-		#Add .table command later
-		try:
-			query = curr.execute(response)
-			for row in query:
-				print(str(row))
-			write_queries(response)
-			
-		except:
-			log_error(f"{response} did not work.")
-		
+			manual_loop(dblist, path)
+		elif response == ".table":
+			sqlite_tables(database)
+		else:
+			# If the response is not a keyword we defined
+			# We will try to run it as a sqlite3 query.
+			try:
+				query = db_exec(database, response)
+				for row in query:
+					print(str(row))
+				write_queries(response)
+				
+			except:
+				log_error(f"{response} did not work.")
+
 
 # Main loop for manual mode.
 # User will input a number of a database.
 # They can then access that db.
-def manual_mode(path):
-	print("Manual Mode selected. Please wait for crawling to finish.")
-	dbs = crawl(path)
-	print("Crawling finished.")
-	if len(dbs) == 0:
-		print("No databases found. Closing program.")
-		sys.exit()
-
+def manual_loop(dbs, path):
 	while True:
 		common_names = display_db(dbs)
 		response = input("Please enter the number of the db you want to access or 'quit' to exit.\n")
@@ -167,9 +180,20 @@ def manual_mode(path):
 			sys.exit()
 		# Need to add checks in the future for if they are entering actual integers.
 		elif int(response) in range(len(dbs)):
-			manual_db(dbs[int(response)], common_names[int(response)], path)
+			manual_db(dbs[int(response)], common_names[int(response)], path, dbs)
 		else:
 			print("Invalid response.")
+
+# Initalizes a list of databases then calls manual_loop.
+def manual_mode(path):
+	print("Manual Mode selected. Please wait for crawling to finish.")
+	dbs = crawl(path)
+	print("Crawling finished.")
+	if len(dbs) == 0:
+		print("No databases found. Closing program.")
+		sys.exit()
+	manual_loop(dbs, path)
+
 
 
 
